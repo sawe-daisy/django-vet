3aa7d61b03909dae080973a645aae23a9dacc2bd
sawe-daisy <deesawe16@gmail.com>
2021-01-17 21:42:28 +0300

    this commit will add veterinary officers list url

--- vets/urls.py
+++ vets/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
-from .views import *
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from . import views
+from .views import VeterinaryViewSet
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns=[
-    path('', welcome, name='welcome')
+    path('', welcome, name='welcome'),
+    path('vets/', VeterinaryViewSet.as_view()),
 ]
