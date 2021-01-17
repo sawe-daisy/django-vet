eabb9e2617c55c4550be06f3a0f85c77a78f99b0
sawe-daisy <deesawe16@gmail.com>
2021-01-17 21:39:43 +0300

    this commit will add veterinary officers list api view

--- vets/views.py
+++ vets/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from urllib import request
+from rest_framework.response import Response
+from rest_framework import generics
+from .serializer import VetSerializer
+from .models import Veterinary
+from rest_framework.decorators import permission_classes
+from django.http import Http404
+from rest_framework.views import APIView
+import json
+from .permissions import *
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 
 def welcome(request):
     return render(request, 'index.html')
+
+@IsAdminUser
+class VeterinaryViewSet(APIView):
+    def get(self, request, format=None):
+        vets=Veterinary.objects.all()
+        serializer=VetSerializer(vets, many=True)
+        return Response(serializer.data)
