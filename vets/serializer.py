3692a2a38d6ddbcd0ae15554d5ee721ccf12c317
sawe-daisy <deesawe16@gmail.com>
2021-01-18 09:32:34 +0300

    this commit will serialize registering users

--- vets/serializer.py
+++ vets/serializer.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import Veterinary
+from django.contrib.auth.models import User
 from rest_framework import viewsets
+
+class UserSerializer(serializers.ModelSerializer):
+    @staticmethod
+    def validate_password(password: str) ->str:
+        return make_password(password)
+    class Meta:
+        model= User
+        fields='__all__'
 
 class VetSerializer(serializers.ModelSerializer):
     class Meta:
